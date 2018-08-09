7a772bf0b9bddcf2b5529fbd1052f1d0412b1587
Ariel <arielrodrigues@protonmail.com>
2018-08-09 11:55:16 -0300

    Ajustes nos arquivos de constantes do projeto

--- App/Themes/Metrics.js
+++ App/Themes/Metrics.js
@@ -5,13 +5,15 @@ const { width, height } = Dimensions.get('window')
 // Used via Metrics.baseMargin
 const metrics = {
   marginHorizontal: 10,
+  doubleMarginHorizontal: 25,
   marginVertical: 10,
-  section: 25,
-  baseMargin: 10,
+  section: 30,
+  baseMargin: 20,
   doubleBaseMargin: 20,
   smallMargin: 5,
   doubleSection: 50,
   horizontalLineHeight: 1,
+  borderRadius: 10,
   screenWidth: width < height ? width : height,
   screenHeight: width < height ? height : width,
   navBarHeight: (Platform.OS === 'ios') ? 64 : 54,
@@ -25,7 +27,7 @@ const metrics = {
   },
   images: {
     small: 20,
-    medium: 40,
+    medium: 50,
     large: 60,
     logo: 200
   }
