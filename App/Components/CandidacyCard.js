5640890f999c8098e609f438d73990bbb9c9bb70
Nélson Rangel <rangelpassos@hotmail.com>
2018-08-17 00:28:40 -0300

    CardContainer agora faz parte do CandidacyCard

--- App/Components/CandidacyCard.js
+++ App/Components/CandidacyCard.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { Images } from '../Themes'
+import CardContainer from './CardContainer'
 import styles from './Styles/CandidacyCardStyle'
 
 type Props = {
@@ -32,47 +33,49 @@ class CandidacyCard extends Component<Props> {
       onPress
     } = this.props
     return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <View style={styles.role}>
-            <Text style={[styles.title, styles.yellow]}>CARGO</Text>
-            <Text style={[styles.headerText, styles.yellow]}>{role}</Text>
+      <CardContainer>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <View style={styles.role}>
+              <Text style={[styles.title, styles.yellow]}>CARGO</Text>
+              <Text style={[styles.headerText, styles.yellow]}>{role}</Text>
+            </View>
+            <View style={styles.party}>
+              <Text style={[styles.title, styles.textRight]}>PARTIDO</Text>
+              <Text style={[styles.headerText, styles.textRight]}>{party}</Text>
+            </View>
           </View>
-          <View style={styles.party}>
-            <Text style={[styles.title, styles.textRight]}>PARTIDO</Text>
-            <Text style={[styles.headerText, styles.textRight]}>{party}</Text>
+          <View style={styles.headerSeparator} />
+          <View style={styles.body}>
+            <View style={styles.info}>
+              <Text style={styles.title}>{electionName}</Text>
+              <Text style={styles.bodyText}>{electionYear}</Text>
+            </View>
+            <View style={styles.info}>
+              <Text style={styles.title}>LOCAL</Text>
+              <Text style={styles.bodyText}>{state ? (city ? `${city}, ${state}` : state) : 'Brasil'}</Text>
+            </View>
+            <View style={styles.info}>
+              <Text style={styles.title}>COLIGAÇÃO</Text>
+              <Text style={styles.bodyText}>{allianceName}</Text>
+              <Text style={styles.bodyText}>{allianceParties}</Text>
+            </View>
+            <View style={styles.info}>
+              <Text style={styles.title}>RESULTADO</Text>
+              <Text style={styles.bodyText}>{result}</Text>
+            </View>
           </View>
+          {result === 'Eleito' && (
+            <View style={styles.performance}>
+              <View style={styles.performanceSeparator} />
+              <TouchableOpacity style={styles.performanceButton} onPress={onPress}>
+                <Text style={[styles.performanceText, styles.textCenter]}>ATUAÇÃO</Text>
+                <Image source={Images.chevronDown} />
+              </TouchableOpacity>
+            </View>
+          )}
         </View>
-        <View style={styles.headerSeparator} />
-        <View style={styles.body}>
-          <View style={styles.info}>
-            <Text style={styles.title}>{electionName}</Text>
-            <Text style={styles.bodyText}>{electionYear}</Text>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.title}>LOCAL</Text>
-            <Text style={styles.bodyText}>{state ? (city ? `${city}, ${state}` : state) : 'Brasil'}</Text>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.title}>COLIGAÇÃO</Text>
-            <Text style={styles.bodyText}>{allianceName}</Text>
-            <Text style={styles.bodyText}>{allianceParties}</Text>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.title}>RESULTADO</Text>
-            <Text style={styles.bodyText}>{result}</Text>
-          </View>
-        </View>
-        {result === 'Eleito' && (
-          <View style={styles.performance}>
-            <View style={styles.performanceSeparator} />
-            <TouchableOpacity style={styles.performanceButton} onPress={onPress}>
-              <Text style={[styles.performanceText, styles.textCenter]}>ATUAÇÃO</Text>
-              <Image source={Images.chevronDown} />
-            </TouchableOpacity>
-          </View>
-        )}
-      </View>
+      </CardContainer>
     )
   }
 }
--- App/Components/CandidacyCard.story.js
+++ App/Components/CandidacyCard.story.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { View } from 'react-native'
 import { storiesOf } from '@storybook/react-native'
 
-import CardContainer from './CardContainer'
 import { Colors, Metrics } from '../Themes'
 
 import CandidacyCard from './CandidacyCard'
@@ -13,32 +12,28 @@ const enhancementStyle = { alignItems: 'center', padding: Metrics.baseMargin, ba
 storiesOf('CandidacyCard', module)
   .addDecorator(storyFn => <View style={enhancementStyle}>{storyFn()}</View>)
   .add('Default', () => (
-    <CardContainer>
-      <CandidacyCard
-        role='Deputado Federal'
-        party='PV'
-        electionName='ELEIÇÕES GERAIS 2014'
-        electionYear='2010'
-        city=''
-        state='Acre'
-        allianceName='Acre Agora e Sempre'
-        allianceParties='PV / PCB / PMDB'
-        result='Eleito'
-      />
-    </CardContainer>
+    <CandidacyCard
+      role='Deputado Federal'
+      party='PV'
+      electionName='ELEIÇÕES GERAIS 2014'
+      electionYear='2010'
+      city=''
+      state='Acre'
+      allianceName='Acre Agora e Sempre'
+      allianceParties='PV / PCB / PMDB'
+      result='Eleito'
+    />
   ))
   .add('Senador', () => (
-    <CardContainer>
-      <CandidacyCard
-        role='Senador'
-        party='REDE'
-        electionName='ELEIÇÕES GERAIS 2014'
-        electionYear='2014'
-        city=''
-        state='Acre'
-        allianceName='Frente Popular de Acrelândia'
-        allianceParties='PT / PMN / PSB / PV / PC do B'
-        result='Não Eleito'
-      />
-    </CardContainer>
+    <CandidacyCard
+      role='Senador'
+      party='REDE'
+      electionName='ELEIÇÕES GERAIS 2014'
+      electionYear='2014'
+      city=''
+      state='Acre'
+      allianceName='Frente Popular de Acrelândia'
+      allianceParties='PT / PMN / PSB / PV / PC do B'
+      result='Não Eleito'
+    />
   ))
