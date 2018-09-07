72cdbdbb90bf679845c18ae537bf6639d4436f9e
Nélson Rangel <rangelpassos@hotmail.com>
2018-09-07 16:49:49 -0300

    improvement: ajustes no layout do cartão de Resumo

--- App/Components/SummaryCard.js
+++ App/Components/SummaryCard.js
@@ -17,16 +17,18 @@ export default class SummaryCard extends React.Component<Props> {
       <CardContainer fetching={fetching}>
         <View style={styles.container}>
           <Text style={styles.subtitle}>PARTIDOS</Text>
-          <View style={[styles.subsection, styles.removeMarginRight]}>
-            <View>
+          <View style={styles.subsection}>
+            <View style={styles.partiesLeftSubsubsection}>
               <Text style={styles.value}>{data.numeroPartidos}</Text>
               <Text style={styles.description}>Partidos</Text>
             </View>
-            <Text style={styles.previousParties}>
-              {data.partidosAnteriores.join(' - ')}
-              {data.partidosAnteriores.length ? ' - ' : null}
-              <Text style={styles.actualParty}>{data.partidoAtual}</Text>
-            </Text>
+            <View style={styles.partiesRightSubsubsection}>
+              <Text style={styles.previousParties}>
+                {data.partidosAnteriores.join(' - ')}
+                {data.partidosAnteriores.length ? ' - ' : null}
+                <Text style={styles.actualParty}>{data.partidoAtual}</Text>
+              </Text>
+            </View>
           </View>
           <Text style={styles.subtitle}>PROCESSOS</Text>
           <View style={styles.subsection}>
@@ -37,22 +39,22 @@ export default class SummaryCard extends React.Component<Props> {
           </View>
           <Text style={styles.subtitle}>ELEIÇÕES</Text>
           <View style={styles.subsection}>
-            <View>
+            <View style={styles.leftSubsubsection}>
               <Text style={styles.value}>{data.numeroCandidaturas}</Text>
               <Text style={styles.description}>Candidaturas</Text>
             </View>
-            <View>
+            <View style={styles.rightSubsubsection}>
               <Text style={styles.value}>{data.numeroMandatos}</Text>
               <Text style={styles.description}>Mandatos</Text>
             </View>
           </View>
           <Text style={styles.subtitle}>AUTORIA DE PROJETOS</Text>
           <View style={styles.subsection}>
-            <View>
+            <View style={styles.leftSubsubsection}>
               <Text style={styles.value}>{data.numeroProposicoes}</Text>
               <Text style={styles.description}>Proposições</Text>
             </View>
-            <View>
+            <View style={styles.rightSubsubsection}>
               <Text style={styles.value}>{data.numeroProjetos}</Text>
               <Text style={styles.description}>Projetos</Text>
             </View>
