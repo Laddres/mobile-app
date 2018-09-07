92fea3f7b6308cc8cfadba9217a0221cb0e51cf5
Nélson Rangel <rangelpassos@hotmail.com>
2018-09-07 14:37:54 -0300

    improvement: ajustes de design na tela principal

--- App/Components/Styles/SectionStyle.js
+++ App/Components/Styles/SectionStyle.js
@@ -3,16 +3,20 @@ import { Colors, Fonts, Metrics } from '../../Themes'
 
 export default StyleSheet.create({
   container: {
-    marginTop: Metrics.section,
-    marginLeft: Metrics.marginHorizontal
+    marginVertical: Metrics.section / 2
   },
   title: {
     fontWeight: 'bold',
     fontSize: Fonts.size.h1,
-    color: Colors.sectionTitle
+    color: Colors.sectionTitle,
+    lineHeight: Fonts.size.h1,
+    marginHorizontal: Metrics.marginHorizontal
   },
   itens: {
     flexDirection: 'row',
     marginTop: Metrics.marginVertical
+  },
+  itensContent: {
+    paddingHorizontal: Metrics.marginHorizontal
   }
 })
