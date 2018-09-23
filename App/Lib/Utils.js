14933566b2422bc8a48ab7b42c02e499d0a1cdbf
Nélson Rangel <rangelpassos@hotmail.com>
2018-09-23 17:27:27 -0300

    Método para retornar uma lista dos estados brasileiros

--- App/Lib/Utils.js
+++ App/Lib/Utils.js
@@ -30,3 +30,141 @@ export const openExternalApp = (url: string) => {
 }
 
 export const generateProjectProposalKey = (id, year, role) => `${id}.${year}.${role}`
+
+export const brazilianStates = () => ({
+  AC: {
+    id: 1,
+    sigla: 'AC',
+    nome: 'Acre'
+  },
+  AL: {
+    id: 2,
+    sigla: 'AL',
+    nome: 'Alagoas'
+  },
+  AM: {
+    id: 3,
+    sigla: 'AM',
+    nome: 'Amazonas'
+  },
+  AP: {
+    id: 4,
+    sigla: 'AP',
+    nome: 'Amapá'
+  },
+  BA: {
+    id: 5,
+    sigla: 'BA',
+    nome: 'Bahia'
+  },
+  CE: {
+    id: 6,
+    sigla: 'CE',
+    nome: 'Ceará'
+  },
+  DF: {
+    id: 7,
+    sigla: 'DF',
+    nome: 'Distrito Federal'
+  },
+  ES: {
+    id: 8,
+    sigla: 'ES',
+    nome: 'Espírito Santos'
+  },
+  GO: {
+    id: 9,
+    sigla: 'GO',
+    nome: 'Goiás'
+  },
+  MA: {
+    id: 10,
+    sigla: 'MA',
+    nome: 'Maranhão'
+  },
+  MG: {
+    id: 11,
+    sigla: 'MG',
+    nome: 'Minas Gerais'
+  },
+  MS: {
+    id: 12,
+    sigla: 'MS',
+    nome: 'Mato Grosso do Sul'
+  },
+  MT: {
+    id: 13,
+    sigla: 'MT',
+    nome: 'Mato Grosso'
+  },
+  PA: {
+    id: 14,
+    sigla: 'PA',
+    nome: 'Pará'
+  },
+  PB: {
+    id: 15,
+    sigla: 'PB',
+    nome: 'Paraíba'
+  },
+  PE: {
+    id: 16,
+    sigla: 'PE',
+    nome: 'Pernambuco'
+  },
+  PI: {
+    id: 17,
+    sigla: 'PI',
+    nome: 'Piauí'
+  },
+  PR: {
+    id: 18,
+    sigla: 'PR',
+    nome: 'Paraná'
+  },
+  RJ: {
+    id: 19,
+    sigla: 'RJ',
+    nome: 'Rio de Janeiro'
+  },
+  RN: {
+    id: 20,
+    sigla: 'RN',
+    nome: 'Rio Grande do Norte'
+  },
+  RO: {
+    id: 21,
+    sigla: 'RO',
+    nome: 'Rondônia'
+  },
+  RR: {
+    id: 22,
+    sigla: 'RR',
+    nome: 'Roraima'
+  },
+  RS: {
+    id: 23,
+    sigla: 'RS',
+    nome: 'Rio Grande do Sul'
+  },
+  SC: {
+    id: 24,
+    sigla: 'SC',
+    nome: 'Santa Catarina'
+  },
+  SE: {
+    id: 25,
+    sigla: 'SE',
+    nome: 'Sergipe'
+  },
+  SP: {
+    id: 26,
+    sigla: 'SP',
+    nome: 'São Paulo'
+  },
+  TO: {
+    id: 27,
+    sigla: 'TO',
+    nome: 'Tocantins'
+  }
+})
