b92799f67200d714d73ea4c96057312d0c30f0f2
Nélson Rangel <rangelpassos@hotmail.com>
2018-09-26 21:29:19 -0300

    fix: por padrão vamos acreditar que o número de mandatos é confiável

--- App/Transforms/SummaryTransforms.js
+++ App/Transforms/SummaryTransforms.js
@@ -8,6 +8,7 @@ export default {
     numeroProcessosJudiciais: rawData['numeroProcessosJudiciais'] || 0,
     numeroCandidaturas: rawData['numeroCandidaturas'] || 0,
     numeroMandatos: rawData['numeroMandatos'] || 0,
+    numeroMandatosPreciso: rawData['numeroMandatosPreciso'] || true,
     numeroProposicoes: rawData['numeroProposicoes'] || 0,
     numeroProjetos: rawData['numeroProjetos'] || 0
   })
