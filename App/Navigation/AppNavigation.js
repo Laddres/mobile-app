6086b7230a41b4f51b42fdb9b655f79476f994e4
Nélson Rangel <rangelpassos@hotmail.com>
2018-09-26 21:25:54 -0300

    feature: nova tela de SplashScreen para aparecer antes do carregamento dos estados internos do app

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -3,12 +3,14 @@ import ResumeScreen from '../Containers/ResumeScreen'
 import MainScreen from '../Containers/MainScreen'
 import WelcomeScreen from '../Containers/WelcomeScreen'
 import StateSelectionScreen from '../Containers/StateSelectionScreen'
+import SplashScreen from '../Containers/SplashScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator(
   {
+    SplashScreen: { screen: SplashScreen },
     WelcomeScreen: { screen: WelcomeScreen },
     StateSelectionScreen: { screen: StateSelectionScreen },
     MainScreen: { screen: MainScreen },
@@ -18,7 +20,7 @@ const PrimaryNav = createStackNavigator(
     // Default config for all screens
     headerMode: 'none',
     cardStyle: { shadowColor: 'transparent' },
-    initialRouteName: 'WelcomeScreen',
+    initialRouteName: 'SplashScreen',
     navigationOptions: {
       headerStyle: styles.header
     }
--- App/Navigation/NavigationConfig.js
+++ App/Navigation/NavigationConfig.js
@@ -3,6 +3,7 @@
  */
 export const DEFAULT_NAVIGATION_CONFIG = {
   exitRoutes: ['MainScreen'],
+  splashScreenRouteName: { routeName: 'SplashScreen' },
   welcomeScreenRouteName: { routeName: 'WelcomeScreen' },
   stateSelectionScreenRouteName: { routeName: 'StateSelectionScreen' },
   mainScreenRouteName: { routeName: 'MainScreen' },
